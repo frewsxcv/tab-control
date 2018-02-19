f79b458179e7b21477d5248e573a572a8c483787
Corey Farwell <coreyf@rwell.org>
2018-02-19 17:29:22 -0500

    move shared code to top of files

--- src/background.js
+++ src/background.js
@@ -1,11 +1,9 @@
+//
+// shared code - keep in-sync in options.js
+//
+
 const browserTypeFirefox = 1;
 const browserTypeChrome = 2;
-
-const storageTabCountKey = 'tab-count';
-
-const badgeBackgroundColorGrey = '#636c72';
-const badgeBackgroundColorYellow = '#f0ad4e';
-const badgeBackgroundColorRed = '#d9534f';
 
 const getBrowser = () => {
     try {
@@ -15,19 +13,39 @@ const getBrowser = () => {
     }
 };
 
-const defaultTabLimit = 10;
-
-const tabLimitStorageKey = "tab-limit";
-
 const [browser, browserType] = getBrowser();
 
-const getMaxNumTabs = () => localStorageGet(tabLimitStorageKey).then(results => {
+const localStorageGet = (keys) => {
+    if (browserType === browserTypeChrome) {
+        return new Promise((resolve, reject) => {
+            browser.storage.local.get(keys, results => {
+                resolve(results);
+            });
+        });
+    } else {
+        return browser.storage.local.get(keys);
+    }
+};
+
+const defaultTabLimit = 10;
+const tabLimitStorageKey = "tab-limit";
+const getTabLimit = () => localStorageGet(tabLimitStorageKey).then(results => {
     return results[tabLimitStorageKey] || defaultTabLimit;
 });
 
+//
+// options code
+//
+
+const storageTabCountKey = 'tab-count';
+
+const badgeBackgroundColorGrey = '#636c72';
+const badgeBackgroundColorYellow = '#f0ad4e';
+const badgeBackgroundColorRed = '#d9534f';
+
 browser.tabs.onCreated.addListener(newTab => {
-    Promise.all([getUnpinnedTabs(), getMaxNumTabs()]).then(([tabs, maxNumTabs]) => {
-        if (tabs.length > maxNumTabs) {
+    Promise.all([getUnpinnedTabs(), getTabLimit()]).then(([tabs, tabLimit]) => {
+        if (tabs.length > tabLimit) {
             browser.tabs.remove(newTab.id);
         }
         setTabCountIntoStorage(tabs.length);
@@ -62,10 +80,10 @@ const onTabCountChange = newTabCount => {
 
 const getBadgeBackgroundColor = tabCount => {
     return new Promise((resolve, reject) => {
-        getMaxNumTabs().then(maxNumTabs => {
-            if (tabCount < maxNumTabs) {
+        getTabLimit().then(tabLimit => {
+            if (tabCount < tabLimit) {
                 return resolve(badgeBackgroundColorGrey);
-            } else if (tabCount === maxNumTabs) {
+            } else if (tabCount === tabLimit) {
                 return resolve(badgeBackgroundColorYellow);
             } else {
                 return resolve(badgeBackgroundColorRed);
@@ -83,17 +101,6 @@ const getTabCountFromStorage = () => {
         .then(results => results[storageTabCountKey]);
 };
 
-const localStorageGet = (keys) => {
-    if (browserType === browserTypeChrome) {
-        return new Promise((resolve, reject) => {
-            browser.storage.local.get(keys, results => {
-                resolve(results);
-            });
-        });
-    } else {
-        return browser.storage.local.get(keys);
-    }
-};
 
 const getTabs = () => {
     if (browserType === browserTypeChrome) {
--- src/options.js
+++ src/options.js
@@ -1,3 +1,7 @@
+//
+// shared code - keep in-sync in background.js
+//
+
 const browserTypeFirefox = 1;
 const browserTypeChrome = 2;
 
@@ -11,14 +15,29 @@ const getBrowser = () => {
 
 const [browser, browserType] = getBrowser();
 
+const localStorageGet = (keys) => {
+    if (browserType === browserTypeChrome) {
+        return new Promise((resolve, reject) => {
+            browser.storage.local.get(keys, results => {
+                resolve(results);
+            });
+        });
+    } else {
+        return browser.storage.local.get(keys);
+    }
+};
+
 const defaultTabLimit = 10;
-
 const tabLimitStorageKey = "tab-limit";
+const getTabLimit = () => localStorageGet(tabLimitStorageKey).then(results => {
+    return results[tabLimitStorageKey] || defaultTabLimit;
+});
 
-/////// TODO: extract above
+//
+// background code
+//
 
 const tabLimitInput = () => document.getElementById("tab-limit-input");
-
 const tabLimitForm = () => document.getElementById("tab-limit-form");
 
 const setTabLimitInputValue = (v) => tabLimitInput().value = v;
@@ -30,11 +49,7 @@ const saveOptions = (e) => {
     });
 };
 
-const restoreOptions = () => {
-    browser.storage.local.get(tabLimitStorageKey, (result) => {
-        setTabLimitInputValue(result[tabLimitStorageKey] || defaultTabLimit);
-    });
-};
+const restoreOptions = () => getTabLimit().then(setTabLimitInputValue);
 
 document.addEventListener("DOMContentLoaded", restoreOptions);
 tabLimitForm().addEventListener("submit", saveOptions);
